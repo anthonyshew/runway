f84995962eddc7d6d09d02285065af5168bc86b7
Anthony Shew <anthonyshew@gmail.com>
2024-01-16 15:01:53 -0700

    Make it work!

--- packages/ui/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
-
-const config: Pick<Config, "prefix" | "presets"> = {
-  prefix: "ui-",
-  presets: [sharedConfig],
-};
-
-export default config;
